c7fdfcbb111337b319515bb68cbebb5770a7947b
Qiangbing Li <qiangbing.li@gmail.com>
2020-07-26 02:09:31 +0300

    coming back after long leave

--- gradient_decent_1d.py
+++ gradient_decent_1d.py
@@ -8,9 +8,9 @@ def yprime(x):
 
 x = 0.44
 eta = 0.1
-loop = 100
+loop = 15
 for i in range(loop):
     x = x - eta*yprime(x)
-    print('%d: %6.3f' % (i, x))
+    print('%d: %6.3f, %6.3f' % (i, x, yprime(x)))
 print('x_min = %6.3f' % (1/math.sqrt(3)))
 print('y_min = %6.3f' % y(x))
